5f2e21ed39f16256190b848f1cea1a82e6b445c0
John Cleary <john@createk.io>
2016-12-27 21:16:20 +0000

    add project-specific routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  
+
+  resources :projects do
+    resources :tests
+    resources :builds 
+  end
+
   namespace :api do
     resources :projects do
       resources :builds
