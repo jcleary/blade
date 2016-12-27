54b97dc0c6578e79454b54a40c4cf5263242af48
John Cleary <john@createk.io>
2016-12-27 17:52:49 +0000

    parse rspec examples

--- app/controllers/api/builds_controller.rb
+++ app/controllers/api/builds_controller.rb
@@ -1,17 +1,23 @@
 class Api::BuildsController < Api::BaseController
   def create
     @project = Project.find params[:project_id]
-    add_tests(params[:build][:examples])
+    @build = @project.builds.create 
+    add_tests(params[:examples])
     
     head :created
   end
 
   private 
 
-  def add_tests(tests)
-    tests.each do |test|
-
-
+  def add_tests(examples)
+    examples.each do |example|
+      test = @project.tests.find_or_create_by(description: example['full_description'])
+      test.name = example['name']
+      test.test_runs.create(
+        build: @build,
+        status: example['status'],
+        duration: example['run_time']
+      )
     end
   end
 
