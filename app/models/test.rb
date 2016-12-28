7af1a3eb0bf400bfbbdb7091d39d69d903076dea
John Cleary <john@createk.io>
2016-12-28 07:04:10 +0000

    calculate the pass ratio

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,12 @@
 class Test < ApplicationRecord
   belongs_to :project
   has_many :test_runs
+
+  def pass_ratio
+    if passed_count + failed_count > 0
+      passed_count / (passed_count + failed_count)
+    else
+      0
+    end
+  end
 end
