88219e001150002fe00474de13797f14956a1ba8
John Cleary <john@createk.io>
2016-12-30 10:39:08 +0000

    add non-deterministic test info the project page

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,12 @@ class Project < ApplicationRecord
   belongs_to :organization
   has_many :builds, dependent: :destroy
   has_many :tests, dependent: :destroy
+
+  def non_deterministic_tests
+    allowed_builds = Build.where('failed_count < 5').pluck(:id)
+    tests.
+      joins(:test_runs).
+      where('test_runs.build_id': allowed_builds).
+      where('test_runs.status': 'failed')
+  end
 end
