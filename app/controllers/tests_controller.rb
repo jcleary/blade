e77d501ef0b8ea7b0a0830a6e5106793c4f4ba82
John Cleary <john@createk.io>
2017-01-01 12:59:05 +0000

    add test show

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -3,5 +3,10 @@ class TestsController < ApplicationController
     @project = Project.find params[:project_id]
     @tests = @project.tests
   end 
+
+  def show
+    @project = Project.find params[:project_id]
+    @test = @project.tests.find params[:id]
+  end
 end
 
